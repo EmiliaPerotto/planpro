c3e29d9caea88e0e7bfe7d22baae2d961281b3af
Emilia Perotto <100068661@csisd.org>
2024-12-09 14:53:51 -0600

    done

--- PlanPro.java
+++ PlanPro.java
@@ -2,58 +2,38 @@ import java.util.*;
 import java.io.*;
 import java.util.ArrayList;
 public class PlanPro{
-  public static void main(String args[]){
-  Scanner in = new Scanner(System.in);
-  ArrayList<String> events = new ArrayList<String>();
-  System.out.println("Hello welcome to your very own virtual planner!");
-  System.out.println("Would you like to view your events or add? (For events type 'events' and for adding type 'add')");
-  String ask = in.nextLine();
-  if(ask.equals("events")){
-  }
-  else if(ask.equals("add")){
-    System.out.println("Below enter the numerical month you would like to add to. (1, 2, 3... 12)");
-    int month = Integer.parseInt(in.nextLine());
-    System.out.println("Below enter the numerical day you would like to add to. (1, 2, 3... 30/31)");
-    int day = Integer.parseInt(in.nextLine());
-    if(month == 1){
-      System.out.println("January " + day);
+  public static void main(String args[])throws IOException{
+  System.out.println("Hello welcome to your very own virtual grader!");
+    Scanner in = new Scanner(System.in);
+    boolean checkFile = false;
+    String filename = " ";
+    System.out.println("What student would you like to see?");
+    while(true){
+    try{
+    filename = in.nextLine();
+    Scanner fin = new Scanner(new File(filename));
+    break;
     }
-    if(month == 2){
-      System.out.println("Febuary " + day);
+    catch(Exception e){
+      System.out.print("Student not found. Enter again: ");
+      continue;
     }
-    if(month == 3){
-      System.out.println("March " + day);
+     }
+    Scanner fin = new Scanner(new File(filename));
+    ArrayList<Integer> studentInfo = new ArrayList<Integer>();
+    int empty = 0;
+    fin.nextLine();
+    while(fin.hasNextLine()){
+      empty = Integer.parseInt(fin.nextLine());
+      studentInfo.add(empty);
+      fin.nextLine();
     }
-    if(month == 4){
-      System.out.println("April" + day);
-    }
-    if(month == 5){
-      System.out.println("May " + day);
-    }
-    if(month == 6){
-      System.out.println("June " + day);
-    }
-    if(month == 7){
-      System.out.println("July " + day);
-    }
-    if(month == 8){
-      System.out.println("August " + day);
-    }
-    if(month == 9){
-      System.out.println("September " + day);
-    }
-    if(month == 10){
-      System.out.println("October " + day);
-    }
-    if(month == 11){
-      System.out.println("November " + day);
-    }
-    if(month == 12){
-      System.out.println("December " + day);
-    }
-  }
-  else{
-    System.out.println("nothing");
+    System.out.println("Math: " + studentInfo.get(0));
+    System.out.println("Science: " + studentInfo.get(1));
+    System.out.println("English: " + studentInfo.get(2));
+    System.out.println("Social Studies: " + studentInfo.get(3));
+    System.out.println("PE: " + studentInfo.get(4));
   }
   }
-  }
+
+
